680ce169f5f77aace73a27c228a8ab7c551eaa74
chris <ckim0707@gmail.com>
2021-08-14 12:16:16 -0500

    NodeJS Week 4 Workshop

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '814599595885957',
-        clientSecret: 'd9277f5bcfd957264b1cc4e63beadea5'
-    }
-}
