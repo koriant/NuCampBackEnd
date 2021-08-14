1dce1e291159d143ad3cb78e53b33cd7a4927e80
chris <ckim0707@gmail.com>
2021-08-14 08:41:51 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '814599595885957',
+        clientSecret: 'd9277f5bcfd957264b1cc4e63beadea5'
+    }
 }
